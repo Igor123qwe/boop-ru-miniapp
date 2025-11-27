167720fd81540257dce7c7b87ebadfe91eac7a86
Igor123qwe <2321158@gmail.com>
2025-11-27 17:36:53 +0300

    update miniapp

--- src/data/popularRoutes/index.ts
+++ src/data/popularRoutes/index.ts
@@ -1,34 +1,19 @@
 // src/data/popularRoutes/index.ts
 import type { PopularRoute } from './types'
 
-// Импортируем весь модуль, не default
-import * as kaliningradModule from './kaliningrad'
-import * as moscowModule from './moscow'
-import * as spbModule from './spb'
-import * as sochiModule from './sochi'
-import * as kazanModule from './kazan'
-
-// Функция: берём первый экспорт из модуля как массив маршрутов
-const getRoutes = (mod: Record<string, unknown>): PopularRoute[] => {
-  const values = Object.values(mod)
-  const first = values[0]
-  return Array.isArray(first) ? (first as PopularRoute[]) : []
-}
-
-// Массивы маршрутов из файлов
-const kaliningrad = getRoutes(kaliningradModule)
-const moscow = getRoutes(moscowModule)
-const spb = getRoutes(spbModule)
-const sochi = getRoutes(sochiModule)
-const kazan = getRoutes(kazanModule)
+import { KALININGRAD_ROUTES } from './kaliningrad'
+import { MOSCOW_ROUTES } from './moscow'
+import { SPB_ROUTES } from './spb'
+import { SOCHI_ROUTES } from './sochi'
+import { KAZAN_ROUTES } from './kazan'
 
 export type { PopularRoute }
 
 // Карта "город → маршруты"
 export const POPULAR_ROUTES: Record<string, PopularRoute[]> = {
-  Калининград: kaliningrad,
-  Москва: moscow,
-  'Санкт-Петербург': spb,
-  Сочи: sochi,
-  Казань: kazan,
+  Калининград: KALININGRAD_ROUTES,
+  Москва: MOSCOW_ROUTES,
+  'Санкт-Петербург': SPB_ROUTES,
+  Сочи: SOCHI_ROUTES,
+  Казань: KAZAN_ROUTES,
 }
